0500272cd5ca9497102f234deb82eeaf9cb8e744
Prathamesh Dabhole <prathameshdabhole06@gmail.com>
2026-06-19 17:55:51 +0530

    MongoDB Atlas configured

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,14 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
+    console.log("MONGO_URI =", process.env.MONGO_URI);
+
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected successfully');
+
+    console.log("MongoDB connected successfully");
   } catch (error) {
-    console.error('MongoDB connection failed:', error.message);
+    console.error("FULL ERROR:", error);
     process.exit(1);
-  }                 
+  }
 };
